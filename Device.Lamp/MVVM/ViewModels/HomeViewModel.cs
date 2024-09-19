6aa6394a5d35351c1c1f54527213f79e29bfb950
JohnoAlm <almenoliver02@gmail.com>
2024-09-19 12:33:52 +0200

    Added _pageTitle observable property and navigation relay command methods to home and settings viewmodel.

--- Device.Lamp/MVVM/ViewModels/HomeViewModel.cs
+++ Device.Lamp/MVVM/ViewModels/HomeViewModel.cs
@@ -1,7 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Device.Lamp.MVVM.ViewModels;
 
 public partial class HomeViewModel : ObservableObject
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public HomeViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    [ObservableProperty]
+    private string _pageTitle = "Home";
+
+    [RelayCommand]
+    private void NavigateToSettings()
+    {
+        var mainWindowModel = _serviceProvider.GetRequiredService<MainWindowModel>(); 
+        mainWindowModel.CurrentViewModel = _serviceProvider.GetRequiredService<SettingsViewModel>();
+    }
 }
--- Device.Lamp/MVVM/ViewModels/SettingsViewModel.cs
+++ Device.Lamp/MVVM/ViewModels/SettingsViewModel.cs
@@ -1,7 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Device.Lamp.MVVM.ViewModels;
 
 public partial class SettingsViewModel : ObservableObject
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public SettingsViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    [ObservableProperty]
+    private string _pageTitle = "Settings";
+
+    [RelayCommand]
+    private void NavigateToHome()
+    {
+        var mainWindowModel = _serviceProvider.GetRequiredService<MainWindowModel>();
+        mainWindowModel.CurrentViewModel = _serviceProvider.GetRequiredService<HomeViewModel>();
+    }
 }

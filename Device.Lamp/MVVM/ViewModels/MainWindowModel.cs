beaeca94f294d5d70f4208f0f34ed72616d6a783
JohnoAlm <almenoliver02@gmail.com>
2024-09-19 10:59:16 +0200

    Added and initialized observable property _currentViewModel to MainWindowModel.

--- Device.Lamp/MVVM/ViewModels/MainWindowModel.cs
+++ Device.Lamp/MVVM/ViewModels/MainWindowModel.cs
@@ -1,7 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Device.Lamp.MVVM.ViewModels;
 
 public partial class MainWindowModel : ObservableObject
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public MainWindowModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        CurrentViewModel = _serviceProvider.GetRequiredService<HomeViewModel>();
+    }
+
+    [ObservableProperty]
+    private ObservableObject _currentViewModel;
 }

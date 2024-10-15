a11a21a824116242b77b5b7444c2ca48b4825996
JohnoAlm <almenoliver02@gmail.com>
2024-10-15 10:54:10 +0200

    Added new converter to disable inputs upon configuring the device.

---
+++ Device.Lamp/Converters/CustomBooleanToIsEnabledConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Device.Lamp.Converters;
+
+public class CustomBooleanToIsEnabledConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is bool isConfigured)
+        {
+            return isConfigured ? false : true;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
